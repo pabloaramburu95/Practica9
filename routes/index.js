4308b862a348c7287044b1fa1fa057338b4a2545
pabloaramburu95 <pabloaramburu95@gmail.com>
2016-05-15 15:27:28 +0200

    Tema 10: Multiples preguntas

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,11 @@ var quizController = require('../controllers/quiz_controller');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
-});
+})
 
-router.get('/question',quizController.question);
-router.get('/check',quizController.check);
-router.get('/author', function(req,res,next){
-	res.render('author');
-});
+//Definicion de rutas de /quizzes
+router.get('/quizzes',quizController.index);
+router.get('/quizzes/:quizId(\\d+)', quizController.show);
+router.get('/quizzes/:quizId(\\d+)/check', quizController.check);
 
 module.exports = router;
