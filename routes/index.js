c4536f47c04f77ef21d80a58253fcdf362da691b
pabloaramburu95 <pabloaramburu95@gmail.com>
2016-05-26 23:31:44 +0200

    Tema 20:Gestion de usuarios

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
+var userController = require('../controllers/user_controller');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
@@ -11,7 +13,16 @@ router.get('/', function(req, res, next) {
 
 // Autoload de rutas que usen :quizId
 router.param('quizId', quizController.load);  // autoload :quizId
+router.param('userId', userController.load); // autoload  :userId
 
+// Definicion de rutas de cuenta
+router.get('/users',			userController.index); //listado de usuarios
+router.get('/users/:userId(\\d+)',      userController.show);    // ver un usuario
+router.get('/users/new',                userController.new);     // formulario sign un
+router.post('/users',                   userController.create);  // registrar usuario
+router.get('/users/:userId(\\d+)/edit', userController.edit);     // editar información de cuenta
+router.put('/users/:userId(\\d+)',      userController.update);   // actualizar información de cuenta
+router.delete('/users/:userId(\\d+)',   userController.destroy);  // borrar cuenta
 
 // Definición de rutas de /quizzes
 router.get('/quizzes.:format?',            quizController.index);
